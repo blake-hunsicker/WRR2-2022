ca710a18f3a03ca504cb38c10dcabe2a06f3e69e
blake-hunsicker <blakeyhunsicker@gmail.com>
2022-04-11 20:55:08 -0400

    Initial

---
+++ script.js
@@ -0,0 +1,57 @@
+function openModal(pageUrl) {
+
+  // Load page into iFrame
+  const modalSiteContainer = document.querySelector('.modal-iframe');
+  modalSiteContainer.setAttribute('src',pageUrl);
+
+  // Open modal
+  const modal = document.querySelector('.modal');
+  modal.classList.toggle('hidden');
+
+  // // Show loading message
+  // if (document.readyState === 'complete') {
+  //   // The page is fully loaded
+  //   console.log('Loaded')
+  // } else {
+  //   // Show loading message
+  // }
+  
+};
+
+// Close modal
+function closeModal() {
+  
+  const modal = document.querySelector('.modal');
+  modal.classList.toggle('hidden');
+  
+};
+
+
+// Flip cards on hover
+
+const bioCardsList = document.querySelectorAll('.bio-card');
+
+let i = 0;
+
+for (i; i < bioCardsList.length; i++) {
+  const bioCard = bioCardsList[i];
+
+  bioCard.addEventListener("mouseenter", function() {
+    const bioCardBack = bioCard.querySelector('.bio-card-back');
+    bioCardBack.classList.remove('hidden');
+
+    const bioCardFront = bioCard.querySelector('.bio-card-front');
+    bioCardFront.classList.add('hidden');
+  });
+  bioCard.addEventListener("mouseleave", function() {
+    const bioCardBack = bioCard.querySelector('.bio-card-back');
+    bioCardBack.classList.add('hidden');
+
+    const bioCardFront = bioCard.querySelector('.bio-card-front');
+    bioCardFront.classList.remove('hidden');
+  });
+}
+
+function flipCard() {
+  
+}
