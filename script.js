b0200bef9e3e418831ff428e11e222474a83fb97
blake-hunsicker <blakeyhunsicker@gmail.com>
2022-04-20 22:56:27 -0400

    Sloppily adding the immersive intro section

--- script.js
+++ script.js
@@ -1,3 +1,61 @@
+// Intro click
+function closeIntroModal() {
+
+  const modal = document.querySelector('.intro-modal');
+  modal.classList.toggle('hidden');
+
+  const body = document.querySelector('body');
+  body.classList.toggle('freeze');
+
+}
+
+// Unmute videos
+function unmute() {
+  const localVideos = document.querySelectorAll('video');
+
+  let i = 0;
+
+  for (i; i < localVideos.length; i++) {
+    localVideos[i].removeAttribute("muted","");
+    localVideos[i].volume = 0.8;
+    console.log("Unmuted video ", i)
+    console.log("Video volume: ", localVideos[i].volume)
+  }
+}
+
+// Autoplay video
+window.addEventListener('load', videoScroll);
+window.addEventListener('scroll', videoScroll);
+
+function videoScroll() {
+  console.log('hi')
+  if ( document.querySelectorAll('video[autoplay]').length > 0) {
+    const windowHeight = window.innerHeight,
+        videoEl = document.querySelectorAll('video[autoplay]');
+
+    for (let i = 0; i < videoEl.length; i++) {
+
+      let thisVideoEl = videoEl[i],
+          videoHeight = thisVideoEl.clientHeight,
+          videoClientRect = thisVideoEl.getBoundingClientRect().top;
+
+      if ( videoClientRect <= ( (windowHeight) - (videoHeight*.15) ) && videoClientRect >= ( 0 - ( videoHeight*.85 ) ) ) {
+        thisVideoEl.play();
+      } else {
+        thisVideoEl.pause();
+      }
+
+      if ( videoClientRect <= ( (windowHeight) - (videoHeight*.25) ) && videoClientRect >= ( 0 - ( videoHeight*.75 ) ) ) {
+        thisVideoEl.classList.add('appear');
+      } else {
+        thisVideoEl.classList.remove('appear');
+      }
+
+    }
+  }
+}
+
+// Open modal
 function openModal(pageUrl) {
 
   // Load page into iFrame
