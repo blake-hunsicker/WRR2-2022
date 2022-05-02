d3f485c451a663008815715dc1001ab839cbaa29
thukdao <102880320+thukdao@users.noreply.github.com>
2022-05-01 20:30:21 -0400

    0501-photogallery-change

--- script.js
+++ script.js
@@ -61,3 +61,38 @@ for (i; i < bioCardsList.length; i++) {
 function flipCard() {
   
 }
+/* Photo gallery */
+
+// (function() {
+//   var scroller = scrollama(),
+//       selector = "div.figure, img, table, video, embed, iframe, .html-widget",
+//       container = document.querySelector(".photo");
+
+//   scroller.setup({"step": ".section, " + selector})
+//     .onStepEnter(res => {
+//       var el = res.element;
+//       el.classList.add("is-active");
+//       var ex = el;
+//       if (el.classList.contains("section")) {
+//         ex = el.querySelector(selector);
+//         if (!ex) {
+//           container.innerHTML = "";
+//           return;
+//         }
+//       }
+//       if (ex.nodeName === "img" && ex.parentNode.classList.contains("figure")) {
+//         ex = ex.parentNode;
+//       }
+//       container.innerHTML = "";
+//       container.append(ex.cloneNode(true));
+//       var els = container.querySelectorAll("*[width]");
+//       for (var i = 0; i < els.length; i++) {
+//         els[i].removeAttribute('width');
+//       }
+//     })
+//     .onStepExit(res => {
+//       res.element.classList.remove("is-active");
+//     });
+
+//   window.addEventListener("resize", scroller.resize);
+// })();
